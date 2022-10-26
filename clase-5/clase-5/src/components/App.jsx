b0c6b30c52a710343c2351879313308808a511d7
Francisco Pugh <franciscopugh01@gmail.com>
2022-10-26 12:05:25 -0300

    Clase 5

---
+++ clase-5/clase-5/src/components/App.jsx
@@ -0,0 +1,15 @@
+import './app.css'
+import Navbar from './Navbar/Navbar';
+import ItemCount from './ItemCount/ItemCount';
+const App = () => {
+  
+  return (
+    <>
+     <Navbar/>
+     <ItemCount/>
+    </>
+   
+  );
+}
+
+export default App;
